926a73932d7aefec0fefecbd692493ef93de066d
X4D0 <46711970+X4D0@users.noreply.github.com>
2020-12-13 10:41:30 +0700

    Update knn_ai.py

--- knn_ai.py
+++ knn_ai.py
@@ -1,8 +1,17 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as  plt
 
-#Read Diabetes.csv
-def readExcel():
-    baca = pd.read_csv(r'Diabetes.csv')
-    print(baca)
+#Create 5 Dataset (Training Set and Testing Set)
 
-readExcel()
+#Importing CSV file
+diabetes = pd.read_csv('Diabetes.csv')
+#print(diabetes.head()) #Showing top 5 data
+
+# Create Variable X and Y
+x = diabetes.drop(['Outcome'], axis = 1) #X untuk seluruh kolom kecuali Outcome
+#print(x.head()) #Showing top 5 data, Outcome not included
+y = diabetes.Outcome.astype('category') #Mengubah kolom Outcome dari bertipe numerik menjadi category
+# category terbagi 2 yaitu 0 = Non-Diabetic, 1 = Diabetic
+#print(y.head()) #Showing top 5 data just the Outcome
+
